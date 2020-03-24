3006d295139ca64347c699e317d88970fd28be44
Dat Dang <46439840+datdang1995@users.noreply.github.com>
2020-03-24 14:00:07 +0700

    Updates

---
+++ precache-manifest.06a2dee0b2a3537f14247fe9f0a7a0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90442000939ad10b09d69fbe6536a9ad",
+    "url": "/practicalassignment/index.html"
+  },
+  {
+    "revision": "b64afcbc21b89ce8faf2",
+    "url": "/practicalassignment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bb1b0678f7cd173930a",
+    "url": "/practicalassignment/static/js/2.ec08ec20.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/practicalassignment/static/js/2.ec08ec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64afcbc21b89ce8faf2",
+    "url": "/practicalassignment/static/js/main.76ac676a.chunk.js"
+  },
+  {
+    "revision": "7e4f6e9ed7b682eb6b9f",
+    "url": "/practicalassignment/static/js/runtime-main.8a31d7b0.js"
+  }
+]);
